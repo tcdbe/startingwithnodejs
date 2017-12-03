f8529c087d5f354aae2924fbf8b6a619be8ce1b2
Tom Coudyzer <tom@tcrscorner.com>
2017-12-03 18:48:56 +0100

    CSV validation
    Added detect-csv to validate if the input / source file is a CSV file

--- assignment_1/csvToJson.js
+++ assignment_1/csvToJson.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const csvConvert = require('csvtojson')
+const validateCSV = require('detect-csv')
 
 if (process.argv.length <= 3) {
     console.log("Usage: node csvToJson.js [csv-file] [json file]")
@@ -9,28 +10,44 @@ if (process.argv.length <= 3) {
 
 let csvFile = path.resolve(process.argv[2])
 let jsonFile =  path.resolve(process.argv[3])
+let isCSV = false
 
-// Absolute path or relative path
+fs.readFile(csvFile,'utf8', function(err,data) {
+    if (err) {
+        throw err;
+    }
+    //Do something with data
+    let isCSV = validateCSV(data)
+    if (isCSV) {
+        let jsonObjects = []
+        
+        csvConvert().fromFile(csvFile)
+                    .on('json',(jsonObj)=>{
+                        
+                        jsonObjects.push(jsonObj)
+                    })            
+                    .on('done', (error) => {
+                        
+                        if (error) {
+                            return console.error(error);
+                        }
+        
+                        let jsonString  = JSON.stringify(jsonObjects, null, 2)
+        
+                        fs.writeFile(jsonFile, jsonString, (error) => {
+                            if (error) {
+                                return console.log(error)
+                            }
+                            console.log("File was saved")
+                        })
+                        console.log("Converion done")
+                    })
+                    
+        } else {
+            console.log("Please supply a valid CSV file")
+        }
+        
+   
+});
 
-
-let jsonObjects = []
-
-csvConvert().fromFile(csvFile)
-            .on('json',(jsonObj)=>{
-                
-                jsonObjects.push(jsonObj)
-            })            
-            .on('done', (error) => {
-         
-
-                let jsonString  = JSON.stringify(jsonObjects, null, 2)
-
-                fs.writeFile(jsonFile, jsonString, (error) => {
-                    if (error) {
-                        return console.log(error)
-                    }
-                    console.log("File was saved")
-                })
-                console.log("Converion done")
-            })
 
