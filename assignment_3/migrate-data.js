ac6693f40ba9cd190785020fe1edf7be40dd1a06
Tom Coudyzer <tom@tcrscorner.com>
2017-12-10 17:59:41 +0100

    Assignment 3 uploaded

---
+++ assignment_3/migrate-data.js
@@ -0,0 +1,46 @@
+const mongodb = require('mongodb')
+const async = require('async')
+
+let mainData = require('./data/m3-customer-data.json')
+let addressData = require('./data/m3-customer-address-data.json')
+
+const url = 'mongodb://localhost:27017/edx-course-db'
+let tasks = []
+
+const numberOfQueries = parseInt(process.argv[2], 10) 
+
+mongodb.MongoClient.connect(url, (error, db) => {
+
+    // If no connection, quit
+    if (error) return process.exit(1)
+    console.log(mainData.length)
+    mainData.forEach((customer, index, list) => {
+        mainData[index] = Object.assign(customer, addressData[index])
+
+        // If a multiple of....
+        if (index % numberOfQueries == 0) {
+            const start = index
+            const end = (start+numberOfQueries > mainData.length) ? mainData.length -1 : start + numberOfQueries
+            // Add to the list of tasks
+            tasks.push((done) => {
+                console.log(`Adding ${start} - ${end} of ${mainData.length}`)
+                db.collection('customers').insert(mainData.slice(start, end), (error, results) => {
+                    done(error, results)
+                })
+            })
+        }
+
+    })
+
+    // Now execute the tasks
+    console.log(`Number of tasks to execute: ${tasks.length}` )
+    const startTime = Date.now()
+    
+    async.parallel(tasks, (error, results) => {
+        if (error) console.error(error)
+        const endTime = Date.now()
+        console.log(`Done in: ${endTime-startTime}`)
+        db.close()
+    })
+    process.exit(1)
+})
