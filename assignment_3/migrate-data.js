2a57b0fdf3b547dd7710193ceeb6c357e1f779f9
Tom Coudyzer <tom@tcrscorner.com>
2017-12-10 19:31:10 +0100

    Improvement - Drop table (if exists), allow no arguments

--- assignment_3/migrate-data.js
+++ assignment_3/migrate-data.js
@@ -7,40 +7,45 @@ let addressData = require('./data/m3-customer-address-data.json')
 const url = 'mongodb://localhost:27017/edx-course-db'
 let tasks = []
 
-const numberOfQueries = parseInt(process.argv[2], 10) 
+const numberOfQueries = parseInt(process.argv[2], 10) || 1000 
 
 mongodb.MongoClient.connect(url, (error, db) => {
 
     // If no connection, quit
     if (error) return process.exit(1)
-    console.log(mainData.length)
-    mainData.forEach((customer, index, list) => {
-        mainData[index] = Object.assign(customer, addressData[index])
-
-        // If a multiple of....
-        if (index % numberOfQueries == 0) {
-            const start = index
-            const end = (start+numberOfQueries > mainData.length) ? mainData.length -1 : start + numberOfQueries
-            // Add to the list of tasks
-            tasks.push((done) => {
-                console.log(`Adding ${start} - ${end} of ${mainData.length}`)
-                db.collection('customers').insert(mainData.slice(start, end), (error, results) => {
-                    done(error, results)
-                })
-            })
-        }
-
-    })
-
-    // Now execute the tasks
-    console.log(`Number of tasks to execute: ${tasks.length}` )
-    const startTime = Date.now()
     
-    async.parallel(tasks, (error, results) => {
-        if (error) console.error(error)
-        const endTime = Date.now()
-        console.log(`Done in: ${endTime-startTime}`)
-        db.close()
-    })
-    process.exit(1)
-})
+    db.collection('customers').drop(function(err, delOK) {
+    if (err) console.error("Error deleting collection customers")
+    if (delOK) {
+        console.log("Collection deleted");
+        mainData.forEach((customer, index, list) => {
+            mainData[index] = Object.assign(customer, addressData[index])
+    
+            // If a multiple of....
+            if (index % numberOfQueries == 0) {
+                const start = index
+                const end = (start+numberOfQueries > mainData.length) ? mainData.length -1 : start + numberOfQueries
+                // Add to the list of tasks
+                tasks.push((done) => {
+                    console.log(`Adding ${start} - ${end} of ${mainData.length}`)
+                    db.collection('customers').insert(mainData.slice(start, end), (error, results) => {
+                        done(error, results)
+                    })
+                })
+            }
+    
+        })
+    
+        // Now execute the tasks
+        console.log(`Number of tasks to execute: ${tasks.length}` )
+        const startTime = Date.now()
+        
+        async.parallel(tasks, (error, results) => {
+            if (error) console.error(error)
+            const endTime = Date.now()
+            console.log(`Done in: ${endTime-startTime}`)
+            db.close()
+        })
+    } 
+  });
+ 
